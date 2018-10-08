8c5669fc33507d0a4c6dafa7a71985caab22e8bb
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-08 17:35:17 -0300

    function create to insert products

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,22 +1,32 @@
 <?php include("cabecalho.php")?>
 <?php
+
+    function insereProduto($conexao, $nome, $preco) {
+        // criando a query
+        $query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
+        return mysqli_query($conexao, $query);
+    }
+
     $nome = $_GET["nome"];
     $preco = $_GET["preco"];
 
     // cria a conexão com o banco de dados
     $conexao = mysqli_connect('localhost', '', '', 'loja');
 
-    // criando a query
-    $query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
-    var_dump(mysql_error($conexao));
 
     // executando a query
-    mysql_query($conexao, $query);
+    if (insereProduto($conexao, $nome, $preco)) { ?>
+        <p class="text-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+    <?php }else { 
+        $msg = mysqli_error($conexao);
+    ?>    
+        <p class="text-danger">Produto <?= $nome; ?>, <?= $preco; ?> não foi adicionado: <?= $msg ?></p>
+    <?php
+    }
 
     // fechando a conexão MySQL
-    mysql_close($conexao);
+    mysqli_close($conexao);
 
     // e com isso o insert into é feito
 ?>
-<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
 <?php include("rodape.php")?>
