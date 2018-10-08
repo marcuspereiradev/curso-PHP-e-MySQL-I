e28862e6442069f413162329a475da3ea20397c7
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-08 17:36:12 -0300

    create navbar

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -9,5 +9,19 @@
     <link rel="stylesheet" href="css/loja.css">
 </head>
 <body>
+
+    <div class="navbar navbar-dark bg-dark navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">Minha loja</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li><a href="produto-formulario.php">Adiciona produto</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="principal">
