f8ddf143a4a5b1821fef0407bd05092b9230bce6
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-09 17:13:45 -0300

    Creating remove, conecta and banco-produto

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -17,6 +17,7 @@
             </div>
             <div>
                 <ul class="nav navbar-nav">
+                    <li><a href="produto-lista.php">Produtos</a></li>
                     <li><a href="produto-formulario.php">Adiciona produto</a></li>
                 </ul>
             </div>
