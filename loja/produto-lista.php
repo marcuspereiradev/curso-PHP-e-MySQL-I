46b595929ba6ff77316528ee9ac3831301afe08e
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-10 16:45:41 -0300

    Adding substr and delete button

--- loja/produto-lista.php
+++ loja/produto-lista.php
@@ -14,7 +14,14 @@
     <tr>
         <td><?=$produto['nome']?></td>
         <td><?=$produto['preco']?></td>
-        <td><a href="remove-produto.php?id=<?=$produto['id']?>" class="text-danger">remover</a></td>
+        <td><?=substr($produto['descricao'], 0, 40)?></td>
+        <td><?= $produto['categoria_nome'] ?></td>
+        <td>
+            <form action="remove-produto.php" method="post">
+                <input type="hidden" name="id" value="<?=$produto['id']?>" />
+                <button class="btn btn-danger">remover</button>
+            </form>
+        </td>
     </tr>
    
     <?php
