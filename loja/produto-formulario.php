ff45229a97d015ea18c1e794f98da3e7c680f166
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-10 16:44:12 -0300

    Changing method of the form and creating view to descrição and categoria

--- loja/produto-formulario.php
+++ loja/produto-formulario.php
@@ -1,6 +1,10 @@
 <?php include("cabecalho.php")?>
+<?php include("conecta.php");?>
+<?php include("banco-categoria.php");?>
+<?php $categorias = listaCategorias($conexao);?>
+
     <h1>Formulário de cadastro</h1>
-    <form action="adiciona-produto.php" method="get">
+    <form action="adiciona-produto.php" method="post">
         <table class="table">
             <tr>
                 <td>Nome</td>   
@@ -11,6 +15,18 @@
                 <td>Preço</td> 
                 <td><input class="form-control" type="number" name="preco"></td>
             </tr>
+
+            <tr>
+                <td>Descrição</td> 
+                <td><textarea name="descricao" class="form-control"></textarea></td>
+            </tr>
+
+            <td>Categoria</td>
+            <td>
+                <?php foreach($categorias as $categoria) : ?>
+                <input type="radio" name="categoria_id" value="<?=$categoria['id']?>"><?=$categoria['nome']?><br>
+                <?php endforeach ?>
+            </td>
 
             <tr>
                 <td><input class="btn-primary" type="submit" value="Cadastrar"></td>
