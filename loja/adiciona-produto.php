6dadc1b225d74be15ff32fe519e31546938e29ae
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-10 16:38:49 -0300

    changing the action GET to POST

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -1,14 +1,16 @@
 <?php include("cabecalho.php")?>
 <?php include("conecta.php")?>
 <?php include("banco-produto.php");?>
+<?php include("banco-categoria.php");?>
 
 <?php
-    $nome = $_GET["nome"];
-    $preco = $_GET["preco"];
-
+    $nome = $_POST["nome"];
+    $preco = $_POST["preco"];
+    $descricao = $_POST["descricao"];
+    $categoria_id = $_POST["categoria_id"];
 
     // executando a query
-    if (insereProduto($conexao, $nome, $preco)) { ?>
+    if (insereProduto($conexao, $nome, $preco, $descricao, $categoria_id)) { ?>
         <p class="text-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
     <?php }else { 
         $msg = mysqli_error($conexao);
