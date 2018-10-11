fed12007a39e90e58b7e7ef2b3f15d5d3bde9fab
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-11 16:22:36 -0300

    Condition to see if  is true or false

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -8,9 +8,14 @@
     $preco = $_POST["preco"];
     $descricao = $_POST["descricao"];
     $categoria_id = $_POST["categoria_id"];
+    if(array_key_exists('usado', $_POST)) {
+        $usado = "true";
+    } else {
+        $usado = "false";
+    }
 
     // executando a query
-    if (insereProduto($conexao, $nome, $preco, $descricao, $categoria_id)) { ?>
+    if (insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado)) { ?>
         <p class="text-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
     <?php }else { 
         $msg = mysqli_error($conexao);
