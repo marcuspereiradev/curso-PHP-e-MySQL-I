da5099d6e613b7f6e12284a1ded815e175c1ea58
marcuspereiragithub <vinnycoast@gmail.com>
2018-10-22 15:56:49 -0300

    Creating page to make contact

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -25,6 +25,7 @@
                 <ul class="nav navbar-nav">
                     <li><a href="produto-lista.php">Produtos</a></li>
                     <li><a href="produto-formulario.php">Adiciona produto</a></li>
+                    <li><a href="contato.php">Contato</a></li>
                 </ul>
             </div>
         </div>
